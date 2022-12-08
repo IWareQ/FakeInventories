94bb43ee02cbddfdfd5c975782881385202e012e
IWareQ <dimonpo96@mail.ru>
2022-12-08 20:01:29 +0400

    Added Workbench, Dispenser, Dropper and Shulker Box inventories

--- src/main/java/me/iwareq/fakeinventories/FakeInventories.java
+++ src/main/java/me/iwareq/fakeinventories/FakeInventories.java
@@ -27,7 +27,11 @@ public class FakeInventories extends PluginBase {
 		FAKE_BLOCKS.put(InventoryType.ENDER_CHEST, new SingleFakeBlock(Block.ENDER_CHEST, BlockEntity.ENDER_CHEST));
 		FAKE_BLOCKS.put(InventoryType.DOUBLE_CHEST, new DoubleFakeBlock(Block.CHEST, BlockEntity.CHEST));
 		FAKE_BLOCKS.put(InventoryType.FURNACE, new SingleFakeBlock(Block.FURNACE, BlockEntity.FURNACE));
+		FAKE_BLOCKS.put(InventoryType.WORKBENCH, new SingleFakeBlock(Block.WORKBENCH, InventoryType.WORKBENCH.getDefaultTitle()));
 		FAKE_BLOCKS.put(InventoryType.BREWING_STAND, new SingleFakeBlock(Block.BREWING_STAND_BLOCK, BlockEntity.BREWING_STAND));
+		FAKE_BLOCKS.put(InventoryType.DISPENSER, new SingleFakeBlock(Block.DISPENSER, InventoryType.DISPENSER.getDefaultTitle()));
+		FAKE_BLOCKS.put(InventoryType.DROPPER, new SingleFakeBlock(Block.DROPPER, InventoryType.DROPPER.getDefaultTitle()));
 		FAKE_BLOCKS.put(InventoryType.HOPPER, new SingleFakeBlock(Block.HOPPER_BLOCK, BlockEntity.HOPPER));
+		FAKE_BLOCKS.put(InventoryType.SHULKER_BOX, new SingleFakeBlock(Block.SHULKER_BOX, BlockEntity.SHULKER_BOX));
 	}
 }
